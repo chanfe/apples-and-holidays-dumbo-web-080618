56e1882afaecaa909ca8c36d8f278b12d4c608fe
Felix Chan <felixchan29@gmail.com>
2018-07-22 03:13:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_arr|
     puts "#{season.capitalize}:"
     holiday_arr.each do |holiday, item|
-      holiday_string = "  #{holiday.capitalize}: "
+      holiday_string = "  #{holiday.capitalize.split("_").join(" ")}: "
       for i in 0..item.length-1
         holiday_string += "#{item[i]}"
         if i != item.length-1
